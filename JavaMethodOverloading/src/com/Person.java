8dcb326b0b3750fc30f497bbf0dd9ff8c29cb4c8
sandipmandalgithub <sandipmandal4055@gmail.com>
2024-02-20 13:18:29 +0530

    Programs on Method Overloading

---
+++ JavaMethodOverloading/src/com/Person.java
@@ -0,0 +1,23 @@
+package com;
+
+public class Person {
+	 void display(int age) {
+		System.out.println("age is:"+age);
+		
+	}
+	void display(double height) {
+		System.out.println("height is:"+height);
+	}
+	void display(String name,int age) {
+		System.out.println("name is :"+name+" and age is :"+age );
+	}
+	 void display(int age,String name) {
+		System.out.println("age is :"+age+" and name is :"+name);
+	}
+
+
+
+
+}
+
+
---
+++ JavaMethodOverloading/src/com/Solution.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Person p=new Person();
+		p.display(20);
+		p.display(5.6);
+		p.display("sandip",20);
+		p.display(20,"sandip");
+
+	}
+
+}
